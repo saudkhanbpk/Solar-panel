f05782330177779a707d488e3ac299fc1b220cab
muhammad zahid khan <mzahidwebdeveloper@gmail.com>
2025-08-12 16:59:49 +0500

    solar pakages pages add

--- src/app/solar-packages/solar-hybrid/page.js
+++ src/app/solar-packages/solar-hybrid/page.js
@@ -1,5 +1,6 @@
+// "use client"
 import React from 'react'
-import SolarSystemCard from '@/Components/SolarSystemCard'
+// import SolarSystem from './Components/SolarSystemCard'
 
 const Hybrid = () => {
   return (
@@ -24,14 +25,7 @@ const Hybrid = () => {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
         <h1 className='text-center text-3xl font-bold text-gray-800 mb-8 border-l-[5px] border-r-[5px] border-gray-800'>Hybrid Solar Systems</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <SolarSystemCard />
-          <SolarSystemCard />
-          <SolarSystemCard />
-          <SolarSystemCard />
-          <SolarSystemCard />
-          <SolarSystemCard />
-          <SolarSystemCard />
-          <SolarSystemCard />
+          {/* <SolarSystem/> */}
         </div>
       </div>
     </div>
